55bbea6e4f2e884a1c20a1e71a08914bffebc482
Khalid Bello <bellokhalid74@gmai.com>
2023-12-12 09:45:27 +0300

    deliver value working fine now

--- routes/views.js
+++ routes/views.js
@@ -12,7 +12,7 @@ router.get('/', (req, res) => {
 });
 
 router.get('/env-test', (req, res)=> {
-  res.send("variables: flutter wave keys: " + process.env.TEST + '  ||||  ' + process.env.NODE_ENV);
+  res.send("variables: flutter wave keys:::: deliver working " + process.env.TEST + '  ||||  ' + process.env.NODE_ENV);
 });
 
 router.get('/test-1', async (req, res) => {
