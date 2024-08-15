3ce661705f05b37a2721f8473364e8da2beead94
CodeNoJutsu <bellokhalid74@gmail.com>
2024-08-15 10:24:08 +0100

    Added log to view open key

--- src/modules/v-account-make-purcchase.ts
+++ src/modules/v-account-make-purcchase.ts
@@ -83,7 +83,7 @@ async function makePurchaseRequest(purchasePayload: any, options: any, bot: stri
         // console.log('response for virtual acount make purchase: ', resp);
 
         if (resp.data.Status === 'successful') return helpSuccesfulDelivery(purchasePayload, resp.data.balance_after, senderId, bot);
-
+        console.log('env keyyyyyyyyyyyyyyyyyyyyyyyy', process.env.OPENSUB_KEY);
         throw { message: 'An error occured delivering data' };
     } catch (error: any) {
         if (error.response) {
