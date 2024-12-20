5d8a171fcd213d2eb652ef57748236535ddabdd7
CodeNoJutsu <bellokhalid74@gmail.com>
2024-12-20 21:15:00 +0100

    debugged auth checker agian

--- src/routes/admin.ts
+++ src/routes/admin.ts
@@ -39,8 +39,9 @@ const adminRouter = Router();
 
 // middle ware to check if user is authenticated
 function authChecker(req: any, res: Response, next: NextFunction) {
-  console.log(req.cookie, 'sessions......');
-  if (req.cookie?.user?.authenticated === true && req.cookies.user?.admin === true) {
+  console.log(req.cookies.user, 'sessions......');
+  const user = JSON.parse(req.cookies.user);
+  if (user.authenticated === true && user.admin === true) {
     return next();
   }
 
