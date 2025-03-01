27ca4b6f9f0a76c549fced6cc55f758b1ac0b38f
CodeNoJutsu <bellokhalid74@gmail.com>
2025-03-01 13:25:12 +0100

    updated mtn 500mb price

--- test.js
+++ test.js
@@ -25,3 +25,35 @@ const   calculateNetworkDataProfit = async ()=> {
 
 
   calculateNetworkDataProfit();
+
+  const axios = require('axios');
+
+const testTermil = ()=> {
+  const data = {
+  "to": "2349166871328",
+  "from": "talert",
+  "sms": "Hi there, testing Termii",
+  "type": "plain",
+  "api_key": "TLsIAbGbPVIGQrigwDbYMEJkPnsTnShZFFZwquYtLRIUckrLZqqiREgGhgeyQk",
+  "channel": "generic",
+  };
+
+const options = {
+  method: 'post',
+  url: 'https://v3.api.termii.com/api/sms/send',
+  headers: {
+    'Content-Type': 'application/json'
+  },
+  data: data // Axios handles JSON stringification automatically
+};
+
+axios(options)
+  .then(response => {
+    console.log(response.data); // Access response.data for the body
+  })
+  .catch(error => {
+    console.error(error);
+  });
+}
+
+//testTermil();
